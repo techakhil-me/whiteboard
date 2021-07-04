9cf470476c99a0d364fad18d19b61b1a644691c5
TechAkhil <43501564+techakhil-me@users.noreply.github.com>
2021-07-04 19:45:10 +0530

    added stroke width, theme toggle, clear all

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("canvas");
+const stroke = document.querySelector(".stroke")
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -36,6 +37,8 @@ function start_draw(e) {
 }
 
 function draw(e) {
+stroke.style.top = `${e.clientY}px`
+stroke.style.left = `${e.clientX}px`
   if (is_drawing && active_tool === "pencil") {
     context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     context.strokeStyle = draw_color;
@@ -64,31 +67,70 @@ const mainBox = document.querySelector("#mainBox");
 const pencilOpen = document.querySelector("#pencilOpen");
 const pencilClose = document.querySelector("#pencilClose");
 const pencilBox = document.querySelector("#pencilBox");
+const eraser = document.querySelector("#eraser");
+const clear = document.querySelector("#clear");
 
 pencilOpen.addEventListener("click", () => {
+  inactive_all();
   mainBox.classList.toggle("slide-out");
   pencilBox.classList.toggle("slide-out");
-  draw_color = document.querySelector(".color-picker").style.color
-  document.querySelector(".color-picker").classList.add('active')
-  active_tool = "pencil"
-
+  draw_color = "black";
+  document.querySelector(".color-picker").classList.add("active");
+  active_tool = "pencil";
 });
 
+document.querySelectorAll(".ri-pencil-fill").forEach((pencil) => {
+  pencil.addEventListener("click", () => {
+    inactive_all();
+    active_tool = "pencil";
+    draw_color = pencil.style.color;
+    pencil.classList.add("active");
+  });
+});
 
-document.querySelectorAll(".ri-pencil-fill").forEach((pencil)=>{
-  pencil.addEventListener('click',()=>{
-    inactive_all()
-    active_tool = "pencil"
-    draw_color = pencil.style.color
-    pencil.classList.add('active')
-  })
-  
-})
 pencilClose.addEventListener("click", () => {
   mainBox.classList.toggle("slide-out");
   pencilBox.classList.toggle("slide-out");
-  
+  active_tool = null;
 });
+
+
+eraser.addEventListener('click',()=>{
+  active_tool = "pencil"
+  inactive_all()
+  draw_color = "white"
+  eraser.classList.toggle("active")
+})
+
+clear.addEventListener('click',()=>{
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+})
+
+
+
+document.body.addEventListener('wheel', function(e) {
+  
+  if (e.deltaY<0 && draw_width<40){
+    draw_width++
+  } else if(draw_width>0){
+    draw_width--
+  }
+  stroke.style.visibility = "visible"
+  stroke.style.width=`${draw_width}px`
+  stroke.style.height=`${draw_width}px`
+  setTimeout(()=>{stroke.style.visibility = "hidden"}, 1000);
+})
+
+
+const theme = document.querySelector("#theme")
+
+theme.addEventListener('click',()=>{
+  theme.classList.toggle("ri-sun-line")
+  theme.classList.toggle("ri-moon-line")
+  document.body.classList.toggle("dark")
+})
 
 window.onresize = () => {
   canvas.width = window.innerWidth;
